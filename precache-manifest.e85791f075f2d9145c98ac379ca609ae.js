6901410a6190e7ab5eb7b2f262854e836480887e
epictrave <flacks12@gmail.com>
2018-11-23 00:12:05 +0900

    Updates

---
+++ precache-manifest.e85791f075f2d9145c98ac379ca609ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "b87ccba6c6d0e98fabf6",
+    "url": "/movie_app/static/js/main.b87ccba6.chunk.js"
+  },
+  {
+    "revision": "0bee8e5dcb3c5e319809",
+    "url": "/movie_app/static/js/1.0bee8e5d.chunk.js"
+  },
+  {
+    "revision": "b87ccba6c6d0e98fabf6",
+    "url": "/movie_app/static/css/main.9b8dad28.chunk.css"
+  },
+  {
+    "revision": "4a50830714741a5247e8a7aab621816d",
+    "url": "/movie_app/index.html"
+  }
+];
